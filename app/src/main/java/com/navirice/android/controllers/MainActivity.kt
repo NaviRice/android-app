f3604e5edf34e4a0e1ff07ed14a65a00d05263aa
Yang Liu <byliuyang11@gmail.com>
2017-12-14 02:26:01 -0500

    Monitor button click

--- app/src/main/java/com/navirice/android/controllers/MainActivity.kt
+++ app/src/main/java/com/navirice/android/controllers/MainActivity.kt
@@ -2,12 +2,15 @@ package com.navirice.android.controllers
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import android.widget.EditText
+import com.jakewharton.rxbinding2.view.RxView
 import com.jakewharton.rxbinding2.widget.RxTextView
 import com.navirice.android.R
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.Observables
+import io.reactivex.rxkotlin.withLatestFrom
 
 /**
  * @author Yang Liu
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private var sourceObservable: Observable<CharSequence>? = null
     private var destinationObservable: Observable<CharSequence>? = null
+    private var startObservable: Observable<Any>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +28,24 @@ class MainActivity : AppCompatActivity() {
 
 
         val textFieldSource: EditText = findViewById(R.id.text_field_source)
-        val textFieldDestination: EditText = findViewById(R.id.text_field_destination)
 
         sourceObservable = RxTextView.afterTextChangeEvents(textFieldSource)
                 .observeOn(AndroidSchedulers.mainThread())
                 .map { event -> event.view().text }
+
+
+        val textFieldDestination: EditText = findViewById(R.id.text_field_destination)
         destinationObservable = RxTextView.afterTextChangeEvents(textFieldDestination)
                 .observeOn(AndroidSchedulers.mainThread())
                 .map { event -> event.view().text }
-        Observables.combineLatest(sourceObservable!!, destinationObservable!!)
+
+        val startButton: Button = findViewById(R.id.start_button);
+        startObservable = RxView.clicks(startButton)
+
+        startObservable!!.withLatestFrom(sourceObservable!!, destinationObservable!!)
                 .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe { input -> println("${input.first} || ${input.second} ") }
+                .subscribe { input -> println("${input.second} || ${input.third}") }
+
 
     }
 }
